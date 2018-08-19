fa3103b242b8e17c942dfb76d45ff874adf9eb33
Cameron Diver <cameron@resin.io>
2018-08-19 23:11:26 +0100

    Add initial test suite

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,25 @@
+import Test.Hspec
+import Test.QuickCheck
+import Control.Exception (evaluate)
+
+import System.IO
+import Data.Either (isRight)
+
+import Compiler.Parser
+
+statementCount :: CodeModule -> Int
+statementCount (CodeModule (StatementList stmts )) = length stmts
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $
+  describe "Parser" $ do
+    describe "Test scripts" $ do
+      it "should parse arithmetic script without error" $ do
+        handle <- openFile "./test/scripts/arithmetic" ReadMode
+        contents <- hGetContents handle
+
+        -- TODO: Don't repeat parseString contents
+        (parseString contents) `shouldSatisfy` isRight
+        let (Right code) = parseString contents
+
+        (statementCount code) `shouldBe` (length $ lines contents)
